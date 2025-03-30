e3df1e310f0a840be0f5d70824b9bff32e27d176
NormanBeltran <josenormanbeltran@gmail.com>
2025-03-29 21:15:20 -0300

    modificacion

--- streamlit_app.py
+++ streamlit_app.py
@@ -84,8 +84,7 @@ st.markdown(
     """
     <style>
     body {
-        background-image: url("https://images.unsplash.com/photo-1495195134817-aeb325a55b65?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
-        background-size: cover;
+        background-color: #90EE90; /* LightGreen color */
     }
     </style>
     """,
